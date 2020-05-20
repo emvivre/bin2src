088297edd951a3c2b15e1f28a1ca4719a3c3dc99
emvivre <emvivre@urdn.com.ua>
2020-05-20 14:24:00 +0200

    * export to .cs

--- bin2src.py
+++ bin2src.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # -*- mode: Python -*-
 
@@ -29,89 +29,108 @@
 import sys
 import os
 
-def output_c_cpp(o_fd, input_file, variable_name_source):
-    o_fd.write(
-'''
-const unsigned char %s[] = {
-''' % variable_name_source)
-    l = 0        
-    f = open(input_file)
+def generate_variable_name_from_source( source ):
+    return source.split('.')[0].replace('+', '_').replace('-', '_')
+
+def generate_hex_array( input_file, space=4 ):
+    s = ''
+    l = 0
+    f = open(input_file, 'rb')
     while True:
         data = f.read(16)
         if len(data) == 0:
             break
         l += len(data)
-        o_fd.write('    ')
+        s += ' ' * space
         for d in data:
-            o_fd.write('0x%02X,' % ord(d))
-        o_fd.write('\n')
-    o_fd.write(
-'''};
+            s += '0x%02X,' % d
+        s += '\n'
+    f.close()
+    return (s,l)
+
+def output_c_cpp(input_file, variable_name_source, output_header):
+    s = ''
+    if len(output_header):
+        s += '''#include "%s"
+
+''' % output_header
+    s += '''const unsigned char %s[] = {
+''' % variable_name_source
+    (ss, l) = generate_hex_array( input_file )
+    s += ss
+    s += '''};
 
 const unsigned int %s_sz = %s;
-''' % (variable_name_source, l))
-    
-def output_assembler(o_fd, input_file, variable_name_source):
+''' % (variable_name_source, l)
+    return s
+
+def output_cs(input_file, variable_name_source):
+    class_name = variable_name_source[0].upper() + variable_name_source[1:]
+    s = '''namespace Project
+{
+    static class %s
+    {
+        public static byte[] %s = {
+''' % (class_name, variable_name_source)
+    (ss, l) = generate_hex_array( input_file, 4*3 )
+    s += ss
+    s += '''        };
+    }
+}'''
+    return s
+
+def output_assembler(input_file, variable_name_source):
     l = os.path.getsize(input_file)
     input_file = os.path.basename(input_file)
-    o_fd.write(
-'''
+    s = '''
     .global %s
     .global %s_sz
 %s:
     .incbin "%s"
 %s_sz:
     .int %d
-''' % (variable_name_source, variable_name_source, variable_name_source, input_file, variable_name_source, l))
-    
-if len(sys.argv) < 4:
-    print 'Usage: %s <INPUT_FILE_0> [<INPUT_FILE_1> ...] <OUTPUT_SOURCE> <OUTPUT_HEADER>' % sys.argv[0]
-    print '   ex: %s /etc/passwd coco.cpp coco.h' % sys.argv[0]
-    print '   ex: %s /etc/passwd coco.S coco.h' % sys.argv[0]
+''' % (variable_name_source, variable_name_source, variable_name_source, input_file, variable_name_source, l)
+    return s
+
+if len(sys.argv) < 3:
+    print('''Usage: %(arg0)s <INPUT_FILE> <OUTPUT_SOURCE> [<OUTPUT_HEADER>]
+with <OUTPUT_SOURCE> : *.s / *.S / *.c / *.cpp / *.cs
+  ex: %(arg0)s /etc/passwd coco.cpp coco.h
+  ex: %(arg0)s /etc/passwd coco.S coco.h''' % {'arg0': sys.argv[0]})
     quit(1)
 
-input_files = sys.argv[1:-2]
-(output_source, output_header) = sys.argv[-2:]
-variable_name = output_header.split('.')[0].replace('+', '_').replace('-', '_')
-variables = []
+(input_file, output_source) = sys.argv[1:3]
+output_header = sys.argv[-1] if len(sys.argv) > 3 else ''
+source_ext = output_source.split('.')[-1]
+variable_name = generate_variable_name_from_source( output_source )
 
-is_assembler = output_source.endswith('.s') or output_source.endswith('.S')
+## export source
+if source_ext in ['s', 'S']:
+    s = output_assembler( input_file, variable_name )
+elif source_ext in ['c', 'cpp']:
+    s = output_c_cpp( input_file, variable_name, output_header )
+elif source_ext == 'cs':
+    s = output_cs( input_file, variable_name )
+else:
+    print('ERROR: invalid output format given !')
+    quit(1)
 
-o_fd = open(output_source, 'w+')
-if is_assembler is False:
-    o_fd.write(
-        '''#include "%s"
-        ''' % output_header)
-for input_file in input_files:
-    if len(input_files) == 1:
-        variable_name_source = variable_name
-    else:
-        variable_name_source = '%s_%s' % (variable_name, os.path.basename(input_file).replace('+', '_').replace('-', '_').replace('.', '_'))
-        
-    if variable_name_source in variables:
-        print 'ERROR: collision of variable name "%s" !!!!' % variable_name_source
-        quit(1)
-    variables.append( variable_name_source )
+with open(output_source, 'w+b') as o_fd:
+    o_fd.write( bytes(s,'utf-8') )
 
-    if is_assembler is True:
-        output_assembler(o_fd, input_file, variable_name_source)
-    else:
-        output_c_cpp(o_fd, input_file, variable_name_source)
-    
-o_fd = open(output_header, 'w+')
-define_name = output_header.replace('.', '_').upper()
-o_fd.write(
-'''#ifndef _%s_
+## export header
+if len(output_header) > 0:
+    s = ''
+    define_name = output_header.replace('.', '_').upper()
+    s += '''#ifndef _%s_
 #define _%s_
-''' % (define_name, define_name))
-for v in variables:
-    o_fd.write(
-'''
+''' % (define_name, define_name)
+    s += '''
 extern const unsigned char %s[];
 extern const unsigned int %s_sz;
-''' % (v, v))
-o_fd.write(
-'''
+''' % (variable_name, variable_name)
+    s += '''
 #endif
-''')
-    
+'''
+    with open(output_header, 'w+b') as o_fd:
+        o_fd.write( bytes(s,'utf-8') )
